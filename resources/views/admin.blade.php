ef804580941f06868032765e2e2e330822ec63b6
Mehdi Boughaleb <bgh84000@gmail.com>
2023-02-01 15:19:36 +0100

    dashboard

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,12 +1,68 @@
 @extends('layouts.app')
 @section('content')
+@vite(['resources/css/dashboard.css'])
 
-hi admin
-    {{ $userCount}}
-    {{$date}} <br>
-    in the last week {{$userCountLastWeek}}<br>
-    les formateur {{$formateurCount}}<br>
-    les etud {{$etudCount}}<br>
-    les users non access {{$usersNonAccess}}<br>
-    les users with access {{$usersAccess}}<br>
+<div class="container">
+    <h1 class="iname"> DASHBOARD</h1>
+        <div class="date">
+            {{$date}}
+
+        </div>
+    <div class="boxs">
+        <div class="box">
+            <div class="top">
+                <p class="heading">Nombre Totale</p>
+                <i class="fa-solid fa-users-line"></i>
+            </div>
+            <div class="bottom">
+               {{ $userCount}}
+            </div>
+        </div>
+        <div class="box">
+            <div class="top">
+                <p class="heading">Nombre formateur</p>
+                <i class="fa-solid fa-chalkboard-user"></i>
+            </div>
+            <div class="bottom">
+               {{$formateurCount}}
+            </div>
+        </div>
+        <div class="box">
+            <div class="top">
+                <p class="heading">Nombre d'étudiants</p>
+                <i class="fa-solid fa-graduation-cap"></i>
+            </div>
+            <div class="bottom">
+               {{$etudCount}}
+            </div>
+        </div>
+        <div class="box">
+            <div class="top">
+                <p class="heading">Nombre last week</p>
+                <i class="fa-solid fa-users"></i>
+            </div>
+            <div class="bottom">
+               {{$userCountLastWeek}}
+            </div>
+        </div>
+        <div class="box">
+            <div class="top">
+                <p class="heading">Nombre d'étudiants<br>access</p>
+                <i class="fa-solid fa-user-check"></i>
+            </div>
+            <div class="bottom">
+                {{$usersAccess}}
+            </div>
+        </div>
+        <div class="box">
+            <div class="top">
+                <p class="heading">Nombre d'étudiants<br>non access</p>
+                <i class="fa-solid fa-user-xmark"></i>
+            </div>
+            <div class="bottom">
+                {{$usersNonAccess}}
+            </div>
+        </div>
+    </div>
+    
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/css/app.css','resources/js/app.js',])
