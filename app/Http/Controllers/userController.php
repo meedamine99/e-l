3baa617d29db819dff4c009f36f42227ded8b468
Mehdi Boughaleb <bgh84000@gmail.com>
2023-02-07 12:48:40 +0100

    show user

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -48,9 +48,9 @@ class userController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(User $user)
     {
-        //
+        return view('users.show', ['user' => $user]);
     }
 
     /**
